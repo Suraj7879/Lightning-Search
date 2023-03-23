1c628f3503613c0b1695f338218059e6d1989f97
suraj7879 <surajsingh7346@gmail.com>
2023-03-23 19:13:17 +0530

    indexing and searching documents

---
+++ search/index.py
@@ -0,0 +1,88 @@
+import math
+from .analysis import analyze
+
+class Index:
+    """
+    Index class that handles indexing and searching documents
+    """
+    def __init__(self):
+        self.index = {}
+        self.documents = {}
+
+    def index_document(self, document):
+        """
+        Index a document by analyzing its fulltext and adding its tokens to the index
+        """
+        if document.ID not in self.documents:
+            self.documents[document.ID] = document
+            document.analyze()
+
+        for token in analyze(document.fulltext):
+            if token not in self.index:
+                self.index[token] = set()
+            self.index[token].add(document.ID)
+
+    def document_frequency(self, token):
+        """
+        Get the frequency of a token in the documents
+        """
+        return len(self.index.get(token, set()))
+
+    def inverse_document_frequency(self, token):
+        """
+        Calculate the inverse document frequency of a token
+        """
+        return math.log10(len(self.documents) / self.document_frequency(token))
+
+    def _results(self, analyzed_query):
+        """
+        Get the results matching the analyzed query tokens
+        """
+        return [self.index.get(token, set()) for token in analyzed_query]
+
+    def search(self, query, search_type='AND', score=False, rank=False):
+        """
+        Search; this will return documents that contain words from the query,
+        and rank them if requested (sets are fast, but unordered).
+
+        Parameters:
+        - query: the query string
+        - search_type: ('AND', 'OR') do all query terms have to match, or just one
+        - score: (True, False) if True, rank results based on TF-IDF score
+        - rank: (True, False) if True, rank results based on score (overrides `score` parameter)
+        """
+        if search_type not in ('AND', 'OR'):
+            return []
+
+        analyzed_query = analyze(query)
+        results = self._results(analyzed_query)
+        if search_type == 'AND':
+            # all tokens must be in the document
+            documents = [self.documents[doc_id] for doc_id in set.intersection(*results)]
+        if search_type == 'OR':
+            # only one token has to be in the document
+            documents = [self.documents[doc_id] for doc_id in set.union(*results)]
+
+        if rank:
+            return self.rank(analyzed_query, documents)
+        elif score:
+            return self.rank_by_score(analyzed_query, documents)
+        else:
+            return documents
+
+
+    def rank(self, analyzed_query, documents):
+        """
+        Rank the documents based on their TF-IDF score
+        """
+        results = []
+        if not documents:
+            return results
+        for document in documents:
+            score = 0.0
+            for token in analyzed_query:
+                tf = document.term_frequency(token)
+                idf = self.inverse_document_frequency(token)
+                score += tf * idf
+            results.append((document, score))
+        return sorted(results, key=lambda doc: doc[1], reverse=True)
